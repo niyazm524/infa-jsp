a07d097046c2b43b3aea794af56c18a8c74d9a3b
Niyaz <niyazm524@gmail.com>
2019-09-26 11:28:12 +0300

    ТУПА ВСЁ

---
+++ out/artifacts/FirstWebApp_war_exploded/KtServlet.kt
@@ -0,0 +1,16 @@
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class KtServlet : HttpServlet() {
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val name = req.getParameter("name")
+        resp.writer.use {
+            it.println(
+                    if (name != null)
+                        "Hello, $name"
+                    else "You didn't provided name, but anyway hello"
+            )
+        }
+    }
+}
